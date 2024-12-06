4e0e8ff3b0eca02e81dfd4117c220f620ae1a863
GabrIel Peres <Gabrielperes99@hotmail.com>
2024-12-06 02:20:24 -0300

    resolve bug knex configs

--- knexfile.js
+++ knexfile.js
@@ -14,15 +14,24 @@ module.exports = {
       min: 2,
       max: 10
     },
-    production: {
-      client: 'mysql2',
-      connection: {
-        host: process.env.DB_HOST_DEPLOY,
-        port: process.env.DB_PORT_DEPLOY,
-        user: process.env.DB_USER_DEPLOY,
-        password: process.env.DB_PASSWORD_DEPLOY,
-        database: process.env.DB_NAME_DEPLOY
-      }
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, './src', './database', './knex', './migrations')
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST_DEPLOY,
+      port: process.env.DB_PORT_DEPLOY,
+      user: process.env.DB_USER_DEPLOY,
+      password: process.env.DB_PASSWORD_DEPLOY,
+      database: process.env.DB_NAME_DEPLOY
+    },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations',
