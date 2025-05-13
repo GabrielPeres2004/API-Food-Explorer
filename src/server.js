2693a0254e39ad4fd9656c1b3218bdb51dbee3cb
GabrIel Peres <Gabrielperes99@hotmail.com>
2025-05-13 16:46:49 -0300

    Update CORS configuration and set cookie expiration to 1 week

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ app.use(
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "https://food-explorer-irnb.onrender.com",
+            "https://www.gabrielperes.site"
         ],
         credentials: true
     })
