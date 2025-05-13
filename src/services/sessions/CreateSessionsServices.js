3e83e48faa06491ceea984701ba013a559053df5
GabrIel Peres <Gabrielperes99@hotmail.com>
2025-05-13 17:14:47 -0300

    Update maxAge cookie

--- src/services/sessions/CreateSessionsServices.js
+++ src/services/sessions/CreateSessionsServices.js
@@ -44,7 +44,7 @@ class CreateSessionsServices {
             httpOnly: true,
             sameSite: "none",
             secure: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 60 * 60 * 1000
         })
 
         return { token, user }
